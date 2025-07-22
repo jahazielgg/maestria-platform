8f5401fe55274dae4598235722fec3c990d1ec91
jahazielgg <u202319831@upc.edu.pe>
2025-07-22 14:42:18 -0500

    feat: add CORS configuration for web application

---
+++ src/main/java/com/maestria/platform/maestriaplatform/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.maestria.platform.maestriaplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos los endpoints
+                        .allowedOrigins("http://localhost:5173") // El puerto de tu frontend (Vite)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
